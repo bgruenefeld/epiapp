39f4c23a55b75fb380950da683e82fe11d8a3f99
Bernd Grünefeld <bernd.gruenefeld@energymeteo.de>
2025-02-12 16:32:48 +0100

    remove unused imports

--- src/pages/CoiCalculator.tsx
+++ src/pages/CoiCalculator.tsx
@@ -1,8 +1,6 @@
-import logo from "../assets/Breedoptimizer.webp"; // Pfad zum großen Bild anpassen
-import { useNavigate } from "react-router-dom"; 
 
 const CoiCalculator: React.FC = () => {
-    const navigate = useNavigate();
+   
     return (
       <div className="container">
          {/* Header mit Logo */}
