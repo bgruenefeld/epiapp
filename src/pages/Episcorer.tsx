125b06a5fc78b8cbfa3fab5168b137708e387fbe
Bernd Grünefeld <bernd.gruenefeld@energymeteo.de>
2025-02-12 16:28:10 +0100

    add backend

---
+++ src/pages/Episcorer.tsx
@@ -0,0 +1,14 @@
+
+const Episcorer: React.FC = () => {
+     
+    return (
+      <div className="container">
+         
+        <h1>Episcorer</h1>
+        <p>Hier kommt die Episcorer-Funktion für die Analyse der Epilepsiebelastung in Ahnentafeln....</p>
+      </div>
+    );
+  };
+  
+  export default Episcorer;
+  
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,49 @@
+import { useNavigate } from "react-router-dom"; 
+import "../App.css";  
+
+const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+            
+        {/* Einleitungstext */}
+      <div className="text-container">
+        Die genetische Vielfalt beim Labrador Retriever nimmt stetig ab,
+        wodurch es für Züchter zunehmend schwieriger wird, gesunde, leistungsfähige und wesensfeste Hunde zu züchten. 
+        Ein enger Genpool kann zu erhöhtem Krankheitsrisiko, 
+        Leistungseinbußen und instabilem Wesen führen – Herausforderungen, mit denen verantwortungsbewusste Züchter täglich konfrontiert sind.
+        Mit BreedOptimizer bieten wir eine datengetriebene Plattform, die Züchtern hilft, nachhaltige und fundierte Zuchtentscheidungen zu treffen. 
+        Basierend auf frei verfügbaren Datenquellen wie K9data.com, Saritas Blog und der DRC-Datenbank entwickeln wir Analysetools, 
+        die den Zuchtprozess intelligenter, effizienter und transparenter gestalten.
+      </div>
+
+      {/* Vorteile */}
+      <div className="text-container">
+        <h3>Was BreedOptimizer Züchtern bietet:</h3>
+        <ul>
+          <li>Genetische Diversitätsanalyse zur Vermeidung von Inzucht und genetischen Engpässen</li>
+          <li>Optimierte Zuchtplanung mit datenbasierten Empfehlungen für die beste Verpaarung</li>
+          <li>Detaillierte Abstammungsanalysen zur besseren Einschätzung von Gesundheits- und Leistungspotenzial</li>
+          <li>Werkzeuge für eine langfristig stabile Zuchtstrategie</li>
+        </ul>
+        Unser Ziel ist es, Züchter mit modernen Methoden zu unterstützen, 
+        damit sie die Zukunft der Labrador-Zucht aktiv und nachhaltig gestalten können.
+
+        <h3>🔗 BreedOptimizer – Weil verantwortungsvolle Zucht Planung erfordert.</h3> 
+      </div>
+      <div className="app-grid">
+        <div className="app-card" onClick={() => navigate("/episcorer")}>
+          <h3>🧬 Episcorer</h3>
+          <p>Analyse der Epilepsiebelastung in Ahnentafeln.</p>
+        </div>
+        <div className="app-card" onClick={() => navigate("/coi-calculator")}>
+          <h3>📊 COI Calculator</h3>
+          <p>Berechnung des Inzuchtkoeffizienten (COI) für Zuchtplanung.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
