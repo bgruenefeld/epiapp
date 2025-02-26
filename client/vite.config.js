4956d38e741517177105b28306882c9380b70217
Bernd Grünefeld <bernd.gruenefeld@energymeteo.de>
2025-02-26 12:29:00 +0100

    increase link font  size

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/epiapp/",
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
     }
+  },
+  staticCopy:{
+    targets: [
+      {
+        src: 'public/data',  // Quelle (relativ zu `client/`)
+        dest: ''  // Ziel: direkt nach `dist/`
+      }
+    ]
   }
 });
 
